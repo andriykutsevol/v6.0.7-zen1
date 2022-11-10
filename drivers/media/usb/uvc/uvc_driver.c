871ba2c18894130a2e433e5b543cc43f0f5bca16
Andriy Kutsevol <andriykutsevol@gmail.com>
2022-11-10 19:31:18 +0200

    c

--- drivers/media/usb/uvc/uvc_driver.c
+++ drivers/media/usb/uvc/uvc_driver.c
@@ -526,8 +526,8 @@ static int uvc_parse_format(struct uvc_device *dev,
 	u32 **intervals, unsigned char *buffer, int buflen)
 {
 	
-	//printk(KERN_INFO "!!!dgnet: uvc_parse_format: dev->udev->dev: %d, buflen: %d 0\n", dev->udev->dev, buflen);
-	uvc_dbg(dev, DESCR,"!!!dgnet uvc_parse_format 0: buflen: %d\n", buflen);
+	printk(KERN_INFO "!!!dgnet: uvc_parse_format: dev->udev->dev: %d, buflen: %d 0\n", dev->udev->dev, buflen);
+	dev_info(&dev, "zzzzzzzzzzzzzzzzzzz\n");
 
 	struct usb_interface *intf = streaming->intf;
 	struct usb_host_interface *alts = intf->cur_altsetting;
